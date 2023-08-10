a48b47e38b5349d0ecf7092c97a016417da9384e
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-08-10 14:13:45 +0200

    feat(core): ObjectMapperConfig for consistent property naming 🐍
    Added PropertyNamingStrategy to ObjectMapperConfig in mindsync app to adopt consistent SNAKE_CASE property naming across the API JSON responses. This change ensures that API consumers have a predictable data structure and promotes easier data processing on the consumer side.

--- apps/mindsync/src/main/kotlin/io/mindsync/config/ObjectMapperConfig.kt
+++ apps/mindsync/src/main/kotlin/io/mindsync/config/ObjectMapperConfig.kt
@@ -2,6 +2,7 @@ package io.mindsync.config
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.json.JsonMapper
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
@@ -25,6 +26,7 @@ class ObjectMapperConfig {
             .build()
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
         mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE)
         mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
 
         val javaTimeModule = JavaTimeModule()
