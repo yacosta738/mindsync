917ebda44b730b03952511842bdab65abbcabe46
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-08-23 09:34:11 +0200

    test(apps/mindsync): 🧪 Added new test methods for JWT Token in AccountResourceService
    Added new test cases for AccountResourceService to verify responses when different authentication methods, specifically JwtAuthenticationToken, are used. Also added checks for handling a scenario when neither OAuth2 nor JWT tokens are used which increases robustness. Changes enhance test coverage and align with best practices for authentication testing.

--- apps/mindsync/src/test/kotlin/io/mindsync/users/infrastructure/service/AccountResourceServiceTest.kt
+++ apps/mindsync/src/test/kotlin/io/mindsync/users/infrastructure/service/AccountResourceServiceTest.kt
@@ -6,10 +6,13 @@ import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User
 import org.springframework.security.oauth2.core.user.OAuth2User
+import org.springframework.security.oauth2.jwt.Jwt
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 
 @UnitTest
 class AccountResourceServiceTest {
@@ -54,6 +57,41 @@ class AccountResourceServiceTest {
         assertTrue(userResponse?.authorities?.contains(AuthoritiesConstants.USER) ?: false)
         assertTrue(userResponse?.authorities?.contains(AuthoritiesConstants.ADMIN) ?: false)
     }
+
+    @Test
+    fun `should get account information successfully by JwtAuthenticationToken`() {
+        val authenticationToken = createMockJwtAuthenticationToken(userDetails)
+        val userResponse = accountResourceService.getAccount(authenticationToken).block()
+        assertEquals("test", userResponse?.username)
+        assertEquals("test@localhost", userResponse?.email)
+        assertEquals("Test", userResponse?.firstname)
+        assertEquals("User", userResponse?.lastname)
+        assertEquals(1, userResponse?.authorities?.size)
+        assertEquals(AuthoritiesConstants.USER, userResponse?.authorities?.first())
+    }
+
+    @Test
+    fun `should get account information successfully with multiple roles by JwtAuthenticationToken`() {
+        userDetails["roles"] = listOf(AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN)
+        val authenticationToken = createMockJwtAuthenticationToken(userDetails)
+        val userResponse = accountResourceService.getAccount(authenticationToken).block()
+        assertEquals("test", userResponse?.username)
+        assertEquals("test@localhost", userResponse?.email)
+        assertEquals("Test", userResponse?.firstname)
+        assertEquals("User", userResponse?.lastname)
+        assertEquals(2, userResponse?.authorities?.size)
+        assertTrue(userResponse?.authorities?.contains(AuthoritiesConstants.USER) ?: false)
+        assertTrue(userResponse?.authorities?.contains(AuthoritiesConstants.ADMIN) ?: false)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when authentication token is not OAuth2 or JWT`() {
+        val authenticationToken = UsernamePasswordAuthenticationToken("test", "test")
+        assertThrows<IllegalArgumentException> {
+            accountResourceService.getAccount(authenticationToken).block()
+        }
+    }
+
     private fun createMockOAuth2AuthenticationToken(userDetails: Map<String, Any>): OAuth2AuthenticationToken {
         val authorities: Collection<GrantedAuthority> =
             listOf<GrantedAuthority>(SimpleGrantedAuthority(AuthoritiesConstants.ANONYMOUS))
@@ -65,5 +103,21 @@ class AccountResourceServiceTest {
         usernamePasswordAuthenticationToken.details = userDetails
         val user: OAuth2User = DefaultOAuth2User(authorities, userDetails, "sub")
         return OAuth2AuthenticationToken(user, authorities, "oidc")
+    }
+
+    private fun createMockJwtAuthenticationToken(userDetails: Map<String, Any>): JwtAuthenticationToken {
+        val jwt = Jwt.withTokenValue("token")
+            .header("alg", "none")
+            .claim("sub", userDetails["sub"])
+            .claim("preferred_username", userDetails["preferred_username"])
+            .claim("email", userDetails["email"])
+            .claim("given_name", userDetails["given_name"])
+            .claim("family_name", userDetails["family_name"])
+            .claim("roles", userDetails["roles"])
+            .build()
+        val authorities: Collection<GrantedAuthority> = AuthorityUtils.createAuthorityList(
+            AuthoritiesConstants.ANONYMOUS
+        )
+        return JwtAuthenticationToken(jwt, authorities)
     }
 }
