532561465ca5dbbf86b7518e3045ee719e5badae
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-09-02 22:41:24 +0200

    test(apps/client): 🧪Extend auth store tests to cover identity and authority
    Extended the set of unit tests for auth store in 'auth.store.spec.ts'. Added tests for setting user identity and testing user authority. This is to ensure the correctness and robustness of our auth store implementation. Also tested for user authentication status and logout action to ensure a secure user experience. Test store code is now more comprehensive covering different aspects of user authentication and validates the behavior of the entire authentication process.

--- apps/mindsync-frontend/apps/client/tests/unit/stores/auth.store.spec.ts
+++ apps/mindsync-frontend/apps/client/tests/unit/stores/auth.store.spec.ts
@@ -1,8 +1,10 @@
 import { beforeEach, describe, expect, it } from 'vitest';
-import { mount } from '@vue/test-utils';
 import { createPinia, setActivePinia } from 'pinia';
 import { AccessToken } from '../../../src/authentication/domain/AccessToken';
 import { useAuthStore } from '../../../src/stores';
+import User from '../../../src/authentication/domain/User';
+import { Authority } from '../../../src/authentication/domain/Authority';
+
 const accessToken: AccessToken = {
   token: 'test',
   expiresIn: 3600,
@@ -14,13 +16,11 @@ const accessToken: AccessToken = {
   scope: 'test',
 };
 
-// const mockRouter = {
-//   push: vitest.fn(),
-// };
 describe('auth store', () => {
   beforeEach(() => {
     setActivePinia(createPinia());
   });
+
   it('should set access token', async () => {
     const authStore = useAuthStore();
     await authStore.setAccessToken(accessToken);
@@ -33,5 +33,68 @@ describe('auth store', () => {
     expect(storedToken.notBeforePolicy).toBe(accessToken.notBeforePolicy);
     expect(storedToken.sessionState).toBe(accessToken.sessionState);
     expect(storedToken.scope).toBe(accessToken.scope);
+  });
+
+  it('should set identity', async () => {
+    const authStore = useAuthStore();
+    const user: User = {
+      id: 'test',
+      username: 'test',
+      email: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      authorities: [Authority.USER],
+    };
+    await authStore.setIdentity(user);
+    const storedUser = authStore.userIdentity;
+    expect(storedUser.id).toBe(user.id);
+    expect(storedUser.username).toBe(user.username);
+    expect(storedUser.email).toBe(user.email);
+    expect(storedUser.firstName).toBe(user.firstName);
+    expect(storedUser.lastName).toBe(user.lastName);
+    storedUser.authorities.forEach((authority, index) => {
+      expect(authority).toBe(user.authorities[index]);
+    });
+  });
+
+  it('should check if user has any authority', async () => {
+    const authStore = useAuthStore();
+    const user: User = {
+      id: 'test',
+      username: 'test',
+      email: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      authorities: [Authority.USER],
+    };
+    await authStore.setIdentity(user);
+    const hasAuthority = await authStore.hasAnyAuthority([Authority.USER]);
+    expect(hasAuthority).toBe(true);
+  });
+
+  it('should check if user is authenticated', async () => {
+    const authStore = useAuthStore();
+    const user: User = {
+      id: 'test',
+      username: 'test',
+      email: 'test',
+      firstName: 'test',
+      lastName: 'test',
+      authorities: [Authority.USER],
+    };
+    await authStore.setIdentity(user);
+    await authStore.setAccessToken(accessToken);
+    await authStore.authenticate('/test-route');
+    expect(authStore.url).toBe('/test-route');
+    expect(authStore.isAuthenticated).toBe(true);
+  });
+
+  it('should logout user', async () => {
+    const authStore = useAuthStore();
+    await authStore.logout();
+    expect(authStore.isAuthenticated).toBe(false);
+    expect(authStore.accessToken).toBe(null);
+    expect(authStore.userIdentity).toBe(null);
+    expect(authStore.url).toBe('/');
   });
 });
