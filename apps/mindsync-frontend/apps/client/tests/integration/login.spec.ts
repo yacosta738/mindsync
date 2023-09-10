1d4e58686cd85e2e3f38fc9f94054a1cb64d44c6
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-09-10 14:04:28 +0200

    refactor(apps/client): login e2e test suit
    The commit encompasses refactored code for the login test suite, aiming to improve reusability and readability. The key changes include modularization of mock routing functions (mockXsrfRoute, mockLoginRoute, mockAccountRoute) which allow for reusable testing framework that handles the common scenario of mocking API routes. This enhances code readability and maintainability by avoiding repetition and improving the structure of the tests.
    
    Additional test cases have been introduced to cover scenarios where login fails due to incorrect credentials, further enhancing the robustness of the test suite.
    
    All changes are intended purely for code quality improvements, with no impact on app-level features.

--- apps/mindsync-frontend/apps/client/tests/integration/login.spec.ts
+++ apps/mindsync-frontend/apps/client/tests/integration/login.spec.ts
@@ -1,38 +1,59 @@
-import { expect, test } from '@playwright/test';
+import { expect, Page, test } from '@playwright/test';
 import { createMockAccessToken } from '../unit/AccessTokenMocks';
 import { createMockUser } from '../unit/UserMocks';
 
-test('login a user in the platform', async ({ page }) => {
+async function mockXsrfRoute(
+  page: Page,
+  status: number = 200,
+  xsrfToken?: string = '8fc494c9-44ff-4517-a8ef-9494f43fac72'
+) {
   await page.route('*/**/api/check-health', (route) => {
     route.fulfill({
-      status: 200,
+      status: status,
       headers: {
-        'Set-Cookie':
-          'XSRF-TOKEN=8fc494c9-44ff-4517-a8ef-9494f43fac72; Path=/; HttpOnly; SameSite=Lax; Max-Age=3600',
+        'Set-Cookie': `XSRF-TOKEN=${xsrfToken}; Path=/; HttpOnly; SameSite=Lax; Max-Age=3600`,
       },
       body: 'OK',
     });
   });
-  const accessToken = createMockAccessToken();
+}
+
+async function mockLoginRoute<T>(
+  page: Page,
+  status: number = 200,
+  responseData: T = createMockAccessToken()
+) {
   await page.route('*/**/api/login', (route) => {
     route.fulfill({
-      status: 200,
+      status: status,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(accessToken),
+      body: JSON.stringify(responseData),
     });
   });
-  const user = createMockUser();
+}
+
+async function mockAccountRoute<T>(
+  page: Page,
+  status: number = 200,
+  responseData: T = createMockUser()
+) {
   await page.route('*/**/api/account', (route) => {
     route.fulfill({
-      status: 200,
+      status: status,
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(user),
+      body: JSON.stringify(responseData),
     });
   });
+}
+
+test('login a user in the platform', async ({ page }) => {
+  await mockXsrfRoute(page);
+  await mockLoginRoute(page);
+  await mockAccountRoute(page);
 
   await page.goto('http://localhost:5173/login');
   expect(await page.title()).toBe('MindSync');
@@ -40,11 +61,11 @@ test('login a user in the platform', async ({ page }) => {
   expect(await emailInput.textContent()).toBe('');
   const passwordInput = page.locator('[id="password"]');
   expect(await passwordInput.textContent()).toBe('');
-  // // fill email field
+  // fill email field
   await emailInput.fill('john.doe@mindsync.com');
-  // // tab to password field
+  // tab to password field
   await page.keyboard.press('Tab');
-  // // fill password field
+  // fill password field
   await passwordInput.fill('S3cr3tP@ssw0rd*123');
 
   await page.getByRole('button', { name: 'Sign in' }).click();
@@ -52,4 +73,34 @@ test('login a user in the platform', async ({ page }) => {
   expect(await page.title()).toBe('MindSync');
   // await to redirect to home page after login
   expect(await page.url()).toBe('http://localhost:5173/');
+});
+
+test('should fail when login a user in the platform with wrong credentials', async ({
+  page,
+}) => {
+  await mockXsrfRoute(page);
+  await mockLoginRoute(page, 401, {
+    message: 'Invalid credentials',
+  });
+  await page.goto('http://localhost:5173/login');
+  expect(await page.title()).toBe('MindSync');
+  const emailInput = await page.locator('[id="email"]');
+  expect(await emailInput.textContent()).toBe('');
+  const passwordInput = page.locator('[id="password"]');
+  expect(await passwordInput.textContent()).toBe('');
+  // fill email field
+  await emailInput.fill('john.doe@mindsync.com');
+  // tab to password field
+  await page.keyboard.press('Tab');
+  // fill password field
+  await passwordInput.fill('S3cr3tP@ssw0rd*123*Wr0ng');
+
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.waitForNavigation();
+  expect(await page.title()).toBe('MindSync');
+  // the user should be in the login page
+  expect(await page.url()).toBe('http://localhost:5173/forbidden');
+  // after 3 seconds the user should be redirected to the login page again
+  await page.waitForTimeout(3000);
+  expect(await page.url()).toBe('http://localhost:5173/login');
 });
