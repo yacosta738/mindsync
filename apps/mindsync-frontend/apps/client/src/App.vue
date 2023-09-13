6163be47b938e4e08adbe0ff07ea5377d3b2a37e
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-09-13 08:47:43 +0200

    refactor(apps/client): 🟣 refactor router before guard

--- apps/mindsync-frontend/apps/client/src/App.vue
+++ apps/mindsync-frontend/apps/client/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { onMounted, provide, shallowRef, type Component } from 'vue';
-import { RouterLink, RouterView } from 'vue-router';
+import { type Component, onMounted, provide, shallowRef } from 'vue';
+import { type RouteLocationNormalized, RouterView } from 'vue-router';
 import { useAuthStore, useXsrfTokenStore } from '@/stores';
 import LoginService from '@/authentication/application/LoginService';
 import AccountService from '@/authentication/application/AccountService';
@@ -17,29 +17,45 @@ const accountService = new AccountService(authStore, refreshTokenService);
 provide('accountService', accountService);
 
 const layout: Component | string = shallowRef('div');
-router.beforeResolve(async (to, from, next) => {
+
+const determineLayout = (to: RouteLocationNormalized) => {
   layout.value = Layouts[to.meta.layout] || 'div';
-  const isPublicRoute = to.matched.some((record) => record.meta.isPublic);
-  if (isPublicRoute) {
+};
+
+const isPublicRoute = (to: RouteLocationNormalized): boolean => {
+  return to.matched.some((record) => record.meta.isPublic);
+};
+
+const fetchAccountInfo = (to: RouteLocationNormalized, callBack: Function) => {
+  accountService.retrieveAccountFromServer().then((account) => {
+    if (account) {
+      callBack();
+    } else {
+      authStore.authenticate(to.path);
+    }
+  });
+};
+
+const checkAuthorityAndNavigate = async (to: RouteLocationNormalized) => {
+  if (to.meta?.authorities && to.meta.authorities.length > 0) {
+    if (!(await authStore.hasAnyAuthority(to.meta.authorities))) {
+      await router.push({ name: 'forbidden' });
+    }
+  }
+};
+
+router.beforeResolve(async (to, _, next) => {
+  determineLayout(to);
+  if (isPublicRoute(to)) {
     next();
     return;
   }
   if (!authStore.isAuthenticated) {
     await authStore.authenticate(to.path);
   } else {
-    accountService.retrieveAccountFromServer().then((account) => {
-      if (account) {
-        next();
-      } else {
-        authStore.authenticate(to.path);
-      }
-    });
+    fetchAccountInfo(to, next);
   }
-  if (to.meta?.authorities && to.meta.authorities.length > 0) {
-    if (!(await authStore.hasAnyAuthority(to.meta.authorities))) {
-      await router.push({ name: 'forbidden' });
-    }
-  }
+  await checkAuthorityAndNavigate(to);
   next();
 });
 provide('app:layout', layout);
@@ -47,12 +63,8 @@ provide('app:layout', layout);
 onMounted(() => {
   xsrfTokenStore.init();
   if (authStore.isAuthenticated) {
-    accountService.retrieveAccountFromServer().then((account) => {
-      if (account) {
-        router.push(authStore.url || '/');
-      } else {
-        authStore.authenticate();
-      }
+    fetchAccountInfo(router.currentRoute.value, () => {
+      router.push(authStore.url || '/');
     });
   }
 });
