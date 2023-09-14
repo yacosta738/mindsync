fe133b72a9c3dd690210a49dd0a05c3f22405e62
Yuniel Acosta Pérez <yunielacosta738@gmail.com>
2023-09-14 22:57:44 +0200

    refactor(ci): change to frontend directory

--- apps/mindsync-frontend/apps/landing-page/playwright.config.ts
+++ apps/mindsync-frontend/apps/landing-page/playwright.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : '50%',
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'blob' : 'html',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
